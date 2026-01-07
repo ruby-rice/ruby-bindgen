5deb6dc4e6805faa1dd05593db9fbb9597a8dfec
cfis <cfis@savagexi.com>
2026-01-07 00:31:47 -0800

    More default value namespace fixes.

--- test/bindings/cpp/default_values-rb.cpp
+++ test/bindings/cpp/default_values-rb.cpp
@@ -7,6 +7,8 @@ Rice::Class rb_cAffine3d;
 Rice::Class rb_cAffine3f;
 Rice::Class rb_cCvMat;
 Rice::Class rb_cCvRange;
+Rice::Class rb_cRect;
+Rice::Class rb_cRect2d;
 
 template<typename Data_Type_T, typename T>
 inline void Vec3_builder(Data_Type_T& klass)
@@ -24,6 +26,18 @@ inline void Affine3_builder(Data_Type_T& klass)
   klass.define_constructor(Constructor<cv::Affine3<T>>()).
     define_constructor(Constructor<cv::Affine3<T>, const typename cv::Affine3<T>::Vec3Type&, const typename cv::Affine3<T>::Vec3Type&>(),
       Arg("translation"), Arg("scale") = static_cast<const typename cv::Affine3<T>::Vec3Type&>(cv::Affine3<T>::Vec3Type::all(1)));
+};
+
+template<typename Data_Type_T, typename T>
+inline void Rect__builder(Data_Type_T& klass)
+{
+  klass.define_constructor(Constructor<cv::Rect_<T>>()).
+    define_constructor(Constructor<cv::Rect_<T>, T, T, T, T>(),
+      Arg("x"), Arg("y"), Arg("width"), Arg("height")).
+    define_attr("x", &cv::Rect_<T>::x).
+    define_attr("y", &cv::Rect_<T>::y).
+    define_attr("width", &cv::Rect_<T>::width).
+    define_attr("height", &cv::Rect_<T>::height);
 };
 void Init_DefaultValues()
 {
@@ -47,5 +61,14 @@ void Init_DefaultValues()
   
   rb_cAffine3d = define_class_under<cv::Affine3<double>>(rb_mCv, "Affine3d").
     define(&Affine3_builder<Data_Type<cv::Affine3<double>>, double>);
+  
+  rb_cRect = define_class_under<cv::Rect_<int>>(rb_mCv, "Rect").
+    define(&Rect__builder<Data_Type<cv::Rect_<int>>, int>);
+  
+  rb_cRect2d = define_class_under<cv::Rect_<double>>(rb_mCv, "Rect2d").
+    define(&Rect__builder<Data_Type<cv::Rect_<double>>, double>);
+  
+  rb_mCv.define_module_function("render", &cv::render,
+    Arg("wnd_rect") = static_cast<const cv::Rect_<double>&>(cv::Rect_<double>(0.0, 0.0, 1.0, 1.0)));
 
 }
--- test/headers/cpp/default_values.hpp
+++ test/headers/cpp/default_values.hpp
@@ -43,4 +43,20 @@ namespace cv
 
   typedef Affine3<float> Affine3f;
   typedef Affine3<double> Affine3d;
+
+  // Test template type in default value (like cv::Rect_<double>)
+  template<typename T>
+  class Rect_
+  {
+  public:
+    Rect_();
+    Rect_(T x, T y, T width, T height);
+    T x, y, width, height;
+  };
+
+  typedef Rect_<int> Rect;
+  typedef Rect_<double> Rect2d;
+
+  // Function with template type default value - should generate cv::Rect_<double>(...) not Rect_<double>(...)
+  void render(const Rect_<double>& wndRect = Rect_<double>(0.0, 0.0, 1.0, 1.0));
 }
