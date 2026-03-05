51fa46d13f7cd78a43957b13f42ea202fda4b7cd
Charlie Savage <cfis@savagexi.com>
2026-03-04 23:04:59 -0800

    Fix style: RubyBindGen -> RubyBindgen in CLI, tabs -> spaces in translation_unit.rb.

--- lib/ruby-bindgen/refinements/translation_unit.rb
+++ lib/ruby-bindgen/refinements/translation_unit.rb
@@ -1,24 +1,24 @@
 module FFI
-	module Clang
-		class TranslationUnit
-			def includes
-				result = Set.new
-				adapter = Proc.new do |included_file, inclusion_stack, include_len, unused|
-					cur_ptr = inclusion_stack
-					include_len.times do
-						location = SpellingLocation.new(Lib::CXSourceLocation.new(cur_ptr))
-						if location.from_main_file?
-							cursor = self.cursor(location)
-							text = cursor.extent.text
-							result << text if text
-						end
-						cur_ptr += Lib::CXSourceLocation.size
- 				  end
-				end
+  module Clang
+    class TranslationUnit
+      def includes
+        result = Set.new
+        adapter = Proc.new do |included_file, inclusion_stack, include_len, unused|
+          cur_ptr = inclusion_stack
+          include_len.times do
+            location = SpellingLocation.new(Lib::CXSourceLocation.new(cur_ptr))
+            if location.from_main_file?
+              cursor = self.cursor(location)
+              text = cursor.extent.text
+              result << text if text
+            end
+            cur_ptr += Lib::CXSourceLocation.size
+          end
+        end
 
-				Lib.get_inclusions(self, adapter, nil)
-				result.to_a
-			end
-		end
-	end
+        Lib.get_inclusions(self, adapter, nil)
+        result.to_a
+      end
+    end
+  end
 end
