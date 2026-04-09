3b05ee69b168619cae9bb95a8b80f92f1c3982b8
Charlie Savage <cfis@savagexi.com>
2026-04-08 23:06:06 -0700

    Revert "Fix coverage tracking to load SimpleCov before application code"
    This reverts commit 4a602da78e5d3844ac2269bc242b3f58bba5d101.

--- Rakefile
+++ Rakefile
@@ -5,6 +5,4 @@
 require "bundler/gem_tasks"
 
 require "minitest/test_task"
-Minitest::TestTask.create do |t|
-  t.test_prelude = 'require "simplecov_setup"'
-end
+Minitest::TestTask.create
--- test/abstract_test.rb
+++ test/abstract_test.rb
@@ -1,5 +1,16 @@
 require 'bundler/setup'
-require_relative 'simplecov_setup'
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  if ENV['CI']
+    require 'simplecov-cobertura'
+    SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+  end
+  SimpleCov.start do
+    add_filter '/test/'
+    track_files 'lib/**/*.rb'
+  end
+end
 
 require 'fileutils'
 require 'minitest/autorun'
